ab199d649a0e3d96680c80ab85e06f0fd0f8210b
ultra-ran <307209868@qq.com>
2017-01-05 19:50:38 +0800

    third

--- js/api.js
+++ js/api.js
@@ -3,40 +3,40 @@
  * 2016/12/27 16:49
  * CESDemo
  */
-(function (G) {
+(function(G) {
     let api = {}
-    let __es = function (target, url, fn) {
+    let __es = function(target, url, fn) {
         let es = target.es = new EventSource(String(url));
-        es.onmessage = function (event) {
+        es.onmessage = function(event) {
             fn && fn(event)
         }
     }
-    __es.close = function (target) {
+    __es.close = function(target) {
         let es = target.es;
         if (es && es.onmessage) {
             es.close(), es = es.onmessage = null, delete es
         }
     }
-    
-    api.use = function (o) {
+
+    api.use = function(o) {
         o = o || {}
         let reg_ip = /(\d+)\.(\d+)\.(\d+)\.(\d+)/,
             reg_http = /http\:\/\/(.+)/;
         // local
-        if (o.server && typeof o.server == 'string' && o.server.match(reg_ip)) {
+        if (o.server && typeof o.server == 'string' && reg_ip.test(o.server)) {
             api.server = 'http://' + o.server
             api.local = true
-            // cloud
-        } else if (o.server && typeof o.server == 'string' && o.server.match(reg_http)) {
+                // cloud
+        } else if (o.server && typeof o.server == 'string' && reg_ip.test(o.server)) {
             // ... hehe, use url directly
             // cloud (use us/cn/auto)
         } else {
             api.server = 'http://' + ({
-                    'us': 'api1',
-                    'cn': 'api2',
-                    'demo': 'demo',
-                    'auto': 'api'
-                }[o.server] || 'api') + '.cassianetworks.com'
+                'us': 'api1',
+                'cn': 'api2',
+                'demo': 'demo',
+                'auto': 'api'
+            }[o.server] || 'api') + '.cassianetworks.com'
         }
         api.developer = o.developer || 'tester'
         api.key = o.key || '10b83f9a2e823c47'
@@ -44,12 +44,12 @@
         api.hub = o.hub
         return api
     }
-    api.oauth2 = function (o) {
+    api.oauth2 = function(o) {
         o = o || {}
-        let next = function (d) {
+        let next = function(d) {
             api.access_token = d || '',
                 api.authorization = 'Bearer ' + (d || ''),
-            o.success && o.success(d),
+                o.success && o.success(d),
                 api.trigger('oauth2', [d])
         }
         if (api.local) {
@@ -58,23 +58,27 @@
             $.ajax({
                 type: 'post',
                 url: api.server + '/oauth2/token',
-                headers: {'Authorization': api.base64_dev_key},
-                data: {"grant_type": "client_credentials"},
-                success: function (data) {
+                headers: {
+                    'Authorization': api.base64_dev_key
+                },
+                data: {
+                    "grant_type": "client_credentials"
+                },
+                success: function(data) {
                     next(data.access_token)
                 }
             })
         }
         return api
     }
-    api.on = function (e, fn) {
+    api.on = function(e, fn) {
         api.on[e] = fn
         if (e == 'notify') {
             api.notify(true)
         }
         return api
     }
-    api.off = function (e) {
+    api.off = function(e) {
         api.on[e] = null
         delete api.on[e]
         if (e == 'notify') {
@@ -82,32 +86,34 @@
         }
         return api
     }
-    api.trigger = function (e, args) {
+    api.trigger = function(e, args) {
         api.on[e] && (typeof api.on[e] == 'function') && (api.on[e].apply(api, args))
         return api
     }
-    
-    api.scan = function (chip) {
+
+    api.scan = function(chip) {
         __es(api.scan, api.server + '/gap/nodes/?event=1&mac=' + api.hub + '&chip=' + (chip || 0) + '&access_token=' + api.access_token,
-            function (event) {
+            function(event) {
                 api.trigger('scan', [api.hub, event.data])
             });
         return api
     };
-    api.scan.close = function () {
+    api.scan.close = function() {
         __es.close(api.scan)
         return api
     };
-    api.conn = function (o) {
+    api.conn = function(o) {
         o = o || {}
         $.ajax({
             type: 'post',
             url: api.server + '/gap/nodes/' + o.node + '/connection?mac=' + (o.hub || api.hub),
-            headers: api.local ? '' : {'Authorization': api.authorization},
+            headers: api.local ? '' : {
+                'Authorization': api.authorization
+            },
             data: {
                 "type": o.type || "public"
             },
-            success: function (data) {
+            success: function(data) {
                 console.log(data)
                 o.success && o.success(o.hub || api.hub, o.node, data)
                 api.trigger('conn', [o.hub || api.hub, o.node, data])
@@ -115,13 +121,15 @@
         })
         return api
     }
-    api.conn.close = function (o) {
+    api.conn.close = function(o) {
         o = o || {}
         $.ajax({
             type: 'delete',
             url: api.server + '/gap/nodes/' + o.node + '/connection?mac=' + (o.hub || api.hub),
-            headers: api.local ? '' : {'Authorization': api.authorization},
-            success: function (data) {
+            headers: api.local ? '' : {
+                'Authorization': api.authorization
+            },
+            success: function(data) {
                 console.log(data)
                 o.success && o.success(o.hub || api.hub, o.node, data)
                 api.trigger('conn.close', [o.hub || api.hub, o.node, data])
@@ -129,71 +137,78 @@
         })
         return api
     }
-    
-    api.devices = function (o) {
+
+    api.devices = function(o) {
         o = o || {}
         $.ajax({
             type: 'get',
             url: api.server + '/gap/nodes/?connection_state=connected&mac=' + (o.hub || api.hub),
-            headers: api.local ? '' : {'Authorization': api.authorization},
-            success: function (data) {
+            headers: api.local ? '' : {
+                'Authorization': api.authorization
+            },
+            success: function(data) {
                 console.log(data)
                 o.success && o.success(data)
             }
         })
         return api
     }
-    
-    api.discover = function (o) {
+
+    api.discover = function(o) {
         o = o || {}
         $.ajax({
             type: 'get',
             url: api.server + '/gatt/nodes/' + o.node + '/services/characteristics/descriptors?mac=' + (o.hub || api.hub),
-            headers: api.local ? '' : {'Authorization': api.authorization},
-            success: function (data) {
+            headers: api.local ? '' : {
+                'Authorization': api.authorization
+            },
+            success: function(data) {
                 console.log(data)
                 o.success && o.success(data)
             }
         })
         return api
     }
-    
-    api.write = function (o) {
+
+    api.write = function(o) {
         o = o || {}
         $.ajax({
             type: 'get',
             url: api.server + '/gatt/nodes/' + o.node + '/handle/' + o.handle + '/value/' + o.value + '/?mac=' + (o.hub || api.hub),
             // headers: {'Authorization': api.authorization},
-            success: function (data) {
+            success: function(data) {
                 o.success && o.success(data)
             }
         })
         return api
     }
-    
-    api.read = function (o) {
+
+    api.read = function(o) {
         o = o || {}
         $.ajax({
             type: 'get',
             url: api.server + '/gatt/nodes/' + o.node + '/handle/' + o.handle + '/value/?mac=' + (o.hub || api.hub),
-            headers: {'Authorization': api.authorization},
-            success: function (data) {
+            headers: {
+                'Authorization': api.authorization
+            },
+            success: function(data) {
                 o.success && o.success(data)
             }
         })
         return api
     }
-    
-    api.notify = function (toggle) {
+
+    api.notify = function(toggle) {
         if (toggle) {
             __es(api.notify, api.server + '/gatt/nodes/?event=1&mac=' + api.hub + '&access_token=' + api.access_token,
-                function (event) {
+                function(event) {
                     api.trigger('notify', [api.hub, event.data])
                 })
         } else {
             __es.close(api.notify)
         }
+        return api
     }
-    
+
     G.api = api
 })(this);
--- js/index.js
+++ js/index.js
@@ -1,25 +1,7 @@
-
-var myChart = echarts.init(document.getElementById('main'));
-
-function randomData() {
-	return parseInt(Math.random() * (210 - 180 + 1) + 180);
-}
-
-function randomDate() {
-	now = new Date(+now + oneDay);
-	return now.toLocaleDateString()
-}
-
+var myChart0 = echarts.init(document.getElementById('acc'));
+var myChart1 = echarts.init(document.getElementById('gyro'));
 var data = [];
 var date = [];
-var now = +new Date(1997, 9, 3);
-var oneDay = 24 * 3600 * 1000;
-var value = Math.random() * 1000;
-for (var i = 0; i < 10; i++) {
-	date.push(randomDate());
-	data.push(randomData());
-}
-
 option = {
 	// title: {
 	//     text: '折线图堆叠'
@@ -41,28 +23,61 @@ option = {
 	yAxis: {
 		type: 'value'
 	},
-	series: [
-		{
-			type: 'line',
-			stack: '总量',
-			data: data
-		}
-	]
+	series: [{
+		type: 'line',
+		stack: '总量',
+		data: data
+	}]
 };
 
-myChart.setOption(option);
+myChart0.setOption(option);
+myChart1.setOption(option);
 
 
-setInterval(function () {
+var body = $('body')
+var hubIP = '',
+	devicesMac = '',
+	reg_ip = /(\d+)\.(\d+)\.(\d+)\.(\d+)/g,
+	reg_mac = /[1-f]{2}\:[1-f]{2}\:[1-f]{2}\:[1-f]{2}\:[1-f]{2}\:[1-f]{2}/gi;
+var dd
+body.delegate('#connect', 'click', function () {
+	//此接口用于连接设备
+	console.log('connect clicked');
+	hubIP = $('#hub_ip').val().trim()
+	devicesMac = $('#device_mac').val().trim()
+	// if (!reg_ip.test(hubIP) || !reg_mac.test(devicesMac))
+	//  {
+	// 	alert('hubIP或者MAC输入错误')
+	// 	return
+	// }
 
-	for (var i = 0; i < 1; i++) {
-		data.shift();
-		date.shift();
-		data.push(randomData());
-		date.push(randomDate());
+	api
+		.use({
+			server: hubIP,
+			hub: ''
+		})
+		.on('notify', dataHandle)
+		.on('conn', writeSpecialValue)
+		.conn({
+			node: devicesMac,
+			type: 'public'
+		})
+
+});
+
+function notify(value) {
+	//收到数据后调用这个接口通知我
+	if (value.type === 'ag') {
+		date.push(value.time);
+		data.push(value.acc[0]);
 	}
+	// data.shift();
+	// date.shift();
+	// data.push(randomData());
+	
 
-	myChart.setOption({
+
+	myChart0.setOption({
 		xAxis: {
 			data: date
 		},
@@ -70,50 +85,19 @@ setInterval(function () {
 			data: data
 		}]
 	});
-}, 1000);
+	myChart1.setOption({
+		xAxis: {
+			data: date
+		},
+		series: [{
+			data: data
+		}]
+	});
 
 
-var body = $('body')
-var hubIP = '',
-	devicesMac = ''
-
-body.delegate('#connect', 'click', function () {
-	//此接口用于连接设备
-	console.log('connect clicked');
-	hubIP = $('#hub_ip').val()
-	devicesMac = $('#device_mac').val()
-
-});
-
-function notify(data) {
-	//收到数据后调用这个接口通知我
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-api
-	.use({
-		server: hubIP,
-		hub: ''
-	})
-	.notify(true)
-	.on('notify', dataHandle)
-	.on('conn', writeSpecialValue)
-	.conn({
-		node: devicesMac,
-		type: 'public'
-	})
 
 var writeSpecialValue = function (e, args) {
 	api.write({
@@ -132,121 +116,119 @@ var writeSpecialValue = function (e, args) {
 		value: '01'
 	})
 }
+
 var dataHandle = function (hub, data) {
-
-
-	var dataParse = function (str) {
-		var splitString = function (str, byte1, byte2) {
-			function sum(arr, index) {
-				var _arr = arr.slice(0, index)
-				if (_arr.length !== 0) {
-					return _arr.reduce(function (prev, cur, index, arr) {
-						return prev + cur
-					})
-				}
-				return 0
-			}
-			var temp = [],
-				byteArr = Array.prototype.slice.call(arguments, 1),
-				length = byteArr.length
-			byteArr = byteArr.map(function (item) {
-				return item * 2
-			})
-			for (var i = 0; i < length; i++) {
-				temp.push(parseInt(str.substr(sum(byteArr, i), byteArr[i]), 16))
-			}
-			return temp
-		}
-
-
-
-		var _data = null,
-			hiData = [],
-			lowData1 = [],
-			lowData2 = [],
-			acc = [],
-			gyro = [],
-			light = [],
-			noise = [],
-			pressure = [],
-			temperature = [],
-			hum = [],
-			sdCard = [],
-			buttonStatus = [],
-			magnetometer = [],
-			magnetometerR = [],
-			ledStatus = [],
-			temp = str.substr(0, 2)
-
-
-		if (temp === '01') {
-			lowData1.push(str.substring(2))
-			lowData1.forEach(function (item, index) {
-				var temp = splitString(item, 4, 1, 4, 4, 4, 1, 1)
-				light.push(temp[0])
-				noise.push(temp[1])
-				pressure.push(temp[2])
-				temperature.push(temp[3])
-				hum.push(temp[4])
-				sdCard.push(temp[5])
-				buttonStatus.push(temp[6])
-			})
-			return {
-				time: new Date().getTime(),
-				type: 'en',
-				light: light[0],
-				noise: noise[0],
-				pressure: pressure[0],
-				hum: hum[0]
-			}
-
-		}
-		if (temp === '02') {
-			lowData2.push(str.substring(2))
-			lowData2.forEach(function (item, index) {
-				var temp = splitString(item, 2, 2, 2, 2, 1)
-				magnetometer.push({
-					x: temp[0],
-					y: temp[1],
-					z: temp[2]
-				})
-				magnetometerR.push(temp[3])
-				ledStatus.push(temp[4])
-			})
-			return {
-				time: new Date().getTime(),
-				type: 'ma',
-				ma: [magnetometer[0].x, magnetometer[0].y, magnetometer[0].z]
-				mar: magnetometerR[0]
-			}
-		}
-
-		hiData.push(str)
-		hiData.forEach(function (item, index) {
-			acc.push({
-				x: parseInt(item.substr(0, 4), 16),
-				y: parseInt(item.substr(4, 4), 16),
-				z: parseInt(item.substr(8, 4), 16)
-			})
-			gyro.push({
-				x: parseInt(item.substr(12, 4), 16),
-				y: parseInt(item.substr(16, 4), 16),
-				z: parseInt(item.substr(20, 4), 16)
-			})
-		})
-		return {
-			time: new Date().getTime(),
-			type: 'ag',
-			acc: [acc[0].x, acc[0].y, acc[0].z]
-			mar: magnetometerR[0]
-		}
-	}
 	if (data !== ':keep-alive') {
 		_data = JSON.parse(data)
-		dataParse(_data)
+		dd = (dataParse(_data.value))
+		notify(dd)
 	}
 }
 
+var dataParse = function (str) {
+	var splitString = function (str, byte1, byte2) {
+		function sum(arr, index) {
+			var _arr = arr.slice(0, index)
+			if (_arr.length !== 0) {
+				return _arr.reduce(function (prev, cur, index, arr) {
+					return prev + cur
+				})
+			}
+			return 0
+		}
+		var temp = [],
+			byteArr = Array.prototype.slice.call(arguments, 1),
+			length = byteArr.length
+		byteArr = byteArr.map(function (item) {
+			return item * 2
+		})
+		for (var i = 0; i < length; i++) {
+			temp.push(parseInt(str.substr(sum(byteArr, i), byteArr[i]), 16))
+		}
+		return temp
+	}
 
 
 
+	var _data = null,
+		hiData = [],
+		lowData1 = [],
+		lowData2 = [],
+		acc = [],
+		gyro = [],
+		light = [],
+		noise = [],
+		pressure = [],
+		temperature = [],
+		hum = [],
+		sdCard = [],
+		buttonStatus = [],
+		magnetometer = [],
+		magnetometerR = [],
+		ledStatus = [],
+		temp = str.substr(0, 2)
+
+
+	if (temp === '01') {
+		lowData1.push(str.substring(2))
+		lowData1.forEach(function (item, index) {
+			var temp = splitString(item, 4, 1, 4, 4, 4, 1, 1)
+			light.push(temp[0])
+			noise.push(temp[1])
+			pressure.push(temp[2])
+			temperature.push(temp[3])
+			hum.push(temp[4])
+			sdCard.push(temp[5])
+			buttonStatus.push(temp[6])
+		})
+		return {
+			time: new Date().getTime(),
+			type: 'en',
+			light: light[0],
+			noise: noise[0],
+			pressure: pressure[0],
+			hum: hum[0],
+			temperature: temperature[0]
+		}
+
+	}
+	if (temp === '02') {
+		lowData2.push(str.substring(2))
+		lowData2.forEach(function (item, index) {
+			var temp = splitString(item, 2, 2, 2, 2, 1)
+			magnetometer.push({
+				x: temp[0],
+				y: temp[1],
+				z: temp[2]
+			})
+			magnetometerR.push(temp[3])
+			ledStatus.push(temp[4])
+		})
+		return {
+			time: new Date().getTime(),
+			type: 'ma',
+			ma: [magnetometer[0].x, magnetometer[0].y, magnetometer[0].z],
+			mar: magnetometerR[0]
+		}
+	}
+
+	hiData.push(str)
+	hiData.forEach(function (item, index) {
+		acc.push({
+			x: parseInt(item.substr(0, 4), 16),
+			y: parseInt(item.substr(4, 4), 16),
+			z: parseInt(item.substr(8, 4), 16)
+		})
+		gyro.push({
+			x: parseInt(item.substr(12, 4), 16),
+			y: parseInt(item.substr(16, 4), 16),
+			z: parseInt(item.substr(20, 4), 16)
+		})
+	})
+	return {
+		time: new Date().getTime(),
+		type: 'ag',
+		acc: [acc[0].x, acc[0].y, acc[0].z],
+		gyro: [gyro[0].x, gyro[0].y, gyro[0].z]
+	}
+}
